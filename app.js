427a67ef1d909daa9162541b71827708f5728256
Stephanie Dennehy <stephanie_dennehy@icloud.com>
2024-08-10 12:46:19 -0400

    modified:   app.js 	modified:   index.html 	modified:   styles.css

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// Select all the toggle buttons
+const toggleButtons = document.querySelectorAll(".faq-toggle");
 
+toggleButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Find the corresponding text paragraph
+    const text = button.closest(".faq").querySelector(".faq-text");
+    const iconPlus = button.querySelector(".iconPlus");
+    const iconMinus = button.querySelector(".iconMinus");
+
+    // Toggle the hidden class on the text paragraph
+    text.classList.toggle("hidden");
+
+    // Toggle the icons
+    iconPlus.classList.toggle("hidden");
+    iconMinus.classList.toggle("hidden");
+  });
+});
